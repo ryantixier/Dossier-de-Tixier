de0c55ba80e52578813447b0050f2a8264f12833
ryantixier <123503138+ryantixier@users.noreply.github.com>
2023-10-17 05:30:31 -0600

    build merge; added content to About page, utilized props for card sections

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,41 +1,55 @@
-import React from "react";
+// IMPORTS
 
+// general
+import React from "react";
+import { useEffect } from "react";
+
+// components
+import AboutCard from "../components/AboutCard";
+
+// animation
 import { motion } from "framer-motion";
+import Aos from "aos";
+import "aos/dist/aos.css";
+
+// props
+const greetings = {
+  header: "GREETINGS",
+  description:
+    "My name is Ryan Tixier (last name pronounced T-C-A); I am a certified, full-stack web developer with a specialty in front-end development. I am also a lovable space-nerd, a Rubik's Cube aficionado, and a curious soul. Welcome to my Portfolio!",
+};
+const aboutMe = {
+  header: "BACKGROUND",
+  description:
+    "I have spent the past ten years pursuing the realms of Education, Mental Healthcare, and the combination thereof. Although I do hold a lot of love for Mental Healthcare in my heart, I decided to make a career change to web development, for which I have nurtured a strong passion.",
+};
+const exploreAway = {
+  header: "EXPLORE AWAY",
+  description:
+    "For an abbreviated showcase of my works, please visit the “Portfolio” page of this site; for a more comprehensive list, you may visit my GitHub profile (linked below) and browse my repositories. Finally, be sure to check out my “Resume” page for a downloadable copy of my resume, as well as an ever-growing list of skills and competencies.",
+};
 
 export default function About() {
+  useEffect(() => {
+    Aos.init();
+  }, []);
+
   return (
     <>
-      <motion.h2
-        className="page-title"
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        transition={{ ease: "easeOut", duration: 2 }}
-      >
-        ABOUT ME
-      </motion.h2>
-
-      <motion.div
-        className="page-content"
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        transition={{
-          duration: 1,
-        }}
-      >
-        <motion.p
-          initial={{ opacity: 0, scale: 0 }}
-          animate={{ opacity: 1, scale: 1 }}
-          transition={{
-            duration: 1,
-            delay: 0.2,
-          }}
-        >
-          Greetings! My name is Ryan Tixier (last name prounounced T-C-A); I am
-          a full-stack web developer, space nerd, and proud owner of a...
-          hybrid. Welcome to my ever-evolving portfolio! I hope you enjoy
-          exploring everything it has to offer.
-        </motion.p>
-      </motion.div>
+      <AboutCard
+        header={greetings.header}
+        description={greetings.description}
+      />
+      <br />
+      <AboutCard header={aboutMe.header} description={aboutMe.description} />
+      <br />
+      <AboutCard
+        header={exploreAway.header}
+        description={exploreAway.description}
+      />
+      <br />
+      <br />
+      <br />
     </>
   );
 }
